52220ddfd671b52f78490929338400793d79dff1
jmsalcido <eljose152@gmail.com>
2016-11-13 22:00:38 -0700

    first table view

---
+++ tableview/firsttableview/firsttableview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firsttableview
+//
+//  Created by Jose Miguel Salcido on 11/13/16.
+//  Copyright © 2016 Jose Miguel Salcido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
